76b06acd7f1a42fcff16532d7b43d9d0eb93c31a
max <vloxomire@gmail.com>
2018-10-25 22:07:01 -0300

    agrego toolbar
    Modificarlo en una sola clase llamada mis herramientas

---
+++ app/src/main/java/com/example/masa_corporal/Toolbar/myToolbar.java
@@ -0,0 +1,10 @@
+package com.example.masa_corporal.Toolbar;
+
+import android.content.Context;
+
+public class myToolbar {
+    public static void toolbar (Context,String){
+        //Pasar a una sola clase utilitarios y los hago staticos
+
+    }
+}
